535a09457e9c82852e92fd7b78518497ed53261d
Helina H <hhargine@tktk.ee>
2023-02-04 12:33:38 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada oma nime
+
+Console.WriteLine("Enter your name:");  //Output
+//string - sõne
+string userName = Console.ReadLine();   //Input
+
+//raknedus tervitab kasutajat nimepidi
+Console.WriteLine("Hello, " + userName + "!");  //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
+
